6b7317b06f0e7d2f6928f35ef5f388232cb4546b
kseniia.tsch <ksenijatschuprina@gmail.com>
2023-09-28 16:57:54 +0300

    Task 1: day of week

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# task1
+# try:
+#     num = int(input("Enter the number of the day of the week (1-7), \nwhere 1 is Monday, and 7 is Sunday:   "))
+#
+#     match num:
+#         case 1:
+#             print("This number stands for Monday!")
+#         case 2:
+#             print("This number stands for Tuesday!")
+#         case 3:
+#             print("This number stands for Wednesday!")
+#         case 4:
+#             print("This number stands for Thursday!")
+#         case 5:
+#             print("This number stands for Friday!")
+#         case 6:
+#             print("This number stands for Saturday!")
+#         case 7:
+#             print("This number stands for Sunday!")
+#         case _:
+#             print("Incorrect number")
+# except ValueError:
+#     print("Please enter only numbers")
+# except Exception as e:
+#     print(f"Error {e}")
+# finally:
+#     print("Have a nice day, whatever day of the week it is:)")
