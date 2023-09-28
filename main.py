d563a01ea4ecf9657ee1024250f8a13cdba0a42b
kseniia.tsch <ksenijatschuprina@gmail.com>
2023-09-28 19:32:16 +0300

    Task 2 (ascending order), two variants

--- main.py
+++ main.py
@@ -25,3 +25,55 @@
 #     print(f"Error {e}")
 # finally:
 #     print("Have a nice day, whatever day of the week it is:)")
+
+# task2 variant1
+# while True:
+#     try:
+#         n1 = int(input("Enter the first number: "))
+#         n2 = int(input("Enter the second number: "))
+#     except ValueError:
+#         print("Enter only numbers, starting from the beginning")
+#         continue
+#
+#     if n1 < n2:
+#         print(f"Ascending order: {n1}, {n2}")
+#         break
+#     elif n1 > n2:
+#         print(f"Ascending order: {n2}, {n1}")
+#         break
+#     elif n1 == n2:
+#         print("Numbers equal")
+#         break
+#     else:
+#         print("Incorrect values")
+#         break
+# print("Calculations finished")
+
+# task2 variant2
+# try:
+#     n1 = 0
+#     n2 = 0
+#     while True:
+#         try:
+#             n1 = int(input("Enter the first number: "))
+#         except ValueError:
+#             print("Enter only numbers, please")
+#             continue
+#         break
+#     while True:
+#         try:
+#             n2 = int(input("Enter the second number: "))
+#         except ValueError:
+#             print("Enter only numbers, please")
+#             continue
+#         break
+#     if n1 < n2:
+#         print(f"Ascending order: {n1}, {n2}")
+#     elif n1 > n2:
+#         print(f"Ascending order: {n2}, {n1}")
+#     elif n1 == n2:
+#         print("Numbers equal")
+#     else:
+#         print("Incorrect values")
+# finally:
+#     print("Calculations finished")
